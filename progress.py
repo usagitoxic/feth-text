f16a44c7f9e62e1fdc367c4c31544deb0e3455d3
bqio <dmitry.mikhaltsov98@gmail.com>
2024-12-02 11:11:33 +0300

    added previous text

--- progress.py
+++ progress.py
@@ -6,7 +6,9 @@ progress = 0
 with open("bundle.csv", encoding="utf-8") as csv_file:
     reader = csv.reader(csv_file)
     for row in reader:
-        if row[3] != "":
+        if len(row) == 4 and row[3] != "":
             progress += 1
         total += 1
-    print(f"Переведено {progress} из {total} строк ({round(progress / total * 100)}%).")
+    print(
+        f"Переведено {progress} из {total} записей ({round(progress / total * 100)}%)."
+    )
