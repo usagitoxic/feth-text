ef8ae97ae371ca72cdefe6c870c90c466e945317
bqio <dmitry.mikhaltsov98@gmail.com>
2025-05-20 17:15:42 +0300

    restored subtitle text, fixed bugs, small gui fixes

--- gui.py
+++ gui.py
@@ -96,7 +96,7 @@ class CSVEditor(QMainWindow):
 
         # Строка для поиска по file_type
         self.file_type_search_line_edit = QLineEdit(self)
-        self.file_type_search_line_edit.setPlaceholderText("file_type")
+        self.file_type_search_line_edit.setPlaceholderText("Поиск по file_type")
 
         # Создаем таймер для задержки при поиске
         self.search_timer = QTimer(self)
