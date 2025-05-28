c9ff7d7f66069be75d448dff0855a7faedb2ea9b
bqio <dmitry.mikhaltsov98@gmail.com>
2025-05-28 14:57:29 +0300

    Рутина, добавлен DLC бандл

--- fill.py
+++ fill.py
@@ -1,26 +1,22 @@
 import csv
 import re
 
-REG_1 = re.compile(r"Mission text message (\d+)")
-REG_2 = re.compile(r"Accepted Quest Character Message (\d+)")
-REG_3 = re.compile(r"カレンダーメッセージ(\d+)")
+REGS = [
+    re.compile(r"Mission text message (\d+)"),
+    re.compile(r"Accepted Quest Character Message (\d+)"),
+    re.compile(r"カレンダーメッセージ(\d+)"),
+    re.compile(r"tTemporarymessage"),
+    re.compile(r"\[9999\]NULL#00＠DummyVoice"),
+]
 
-with open("bundle.csv", "r", encoding="utf-8") as f:
-    with open("bundle_o.csv", "w", encoding="utf-8", newline="") as f2:
+with open("dlc.csv", "r", encoding="utf-8") as f:
+    with open("dlc_o.csv", "w", encoding="utf-8", newline="") as f2:
         reader = csv.reader(f)
         writer = csv.writer(f2)
         for row in reader:
-            res1 = REG_1.match(row[2])
-            res2 = REG_2.match(row[2])
-            res3 = REG_3.match(row[2])
-
-            if res1:
-                print(res1.group(1))
-                row[3] = row[2]
-            if res2:
-                print(res2.group(1))
-                row[3] = row[2]
-            if res3:
-                print(res3.group(1))
-                row[3] = row[2]
+            for reg in REGS:
+                res = reg.match(row[2])
+                if res:
+                    print(res)
+                    row[3] = row[2]
             writer.writerow(row)
